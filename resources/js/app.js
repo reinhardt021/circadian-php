bea3b6f294d5cf375b9d5f15102e6b3116937f16
reinhardtc <reinhardt.cgr@gmail.com>
2020-04-20 17:41:51 +1200

    created JS build with laravel-mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,10 +16,14 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+const files = require.context('./', true, /\.vue$/i);
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default
+);
+console.log('>>> test that this is converted');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
