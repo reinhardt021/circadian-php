c1972ec893fb2176b08c5c1757e62eb76b169c7f
reinhardtc <reinhardt.cgr@gmail.com>
2020-04-23 11:10:55 +1200

    Added Test for Flow show action API

--- tests/Feature/FlowTest.php
+++ tests/Feature/FlowTest.php
@@ -14,7 +14,7 @@ class FlowTest extends TestCase
     // todo: create a separate test database with seeded data to test full features .env.test
 
     /**
-     * A Flow GET API test
+     * A GET Flow resources API test
      *
      * @return void
      */
@@ -70,7 +70,37 @@ class FlowTest extends TestCase
         );
     }
 
-//    public function testFlowShow()
+    /**
+     * A GET individual Flow resource API test
+     *
+     * @return void
+     */
+    public function testFlowShow()
+    {
+        // ARRANGE
+        $flows = factory(Flow::class, 1)->create();
+        $flow = $flows[0];
+        $uri = '/api/flows/' . $flow->id;
+        $expectedStructure = [
+            'message',
+            'data' => [
+                'id',
+                'title',
+                'created_at',
+                'updated_at',
+                'deleted_at',
+            ],
+        ];
+
+        // ACT
+        $response = $this->get($uri);
+        $json = $response->json();
+
+        //ASSERT
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure($expectedStructure);
+        $this->assertEquals($flow->id, $json['data']['id']);
+    }
 //    public function testFlowUpdate()
 //    public function testFlowDestroy()
 
