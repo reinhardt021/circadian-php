db60edacb75e71f15e24622226d4b44801e28eed
reinhardtc <reinhardt.cgr@gmail.com>
2020-04-24 13:32:26 +1200

    Have Seeder working for default Pomodoro flow

--- database/factories/TaskOrderFactory.php
+++ database/factories/TaskOrderFactory.php
@@ -3,9 +3,10 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
+use App\TaskOrder;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(TaskOrder::class, function (Faker $faker) {
     return [
         'flow_id' => null,
         'position' => $faker->numberBetween(1, 99),
